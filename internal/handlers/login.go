9e1b0eb9b0975080f2cb445a582ef618c03acd5d
wellitonscheer <wellitonscheer@gmail.com>
2025-02-07 17:59:36 -0300

    feat: disable button when sent

--- internal/handlers/login.go
+++ internal/handlers/login.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/wellitonscheer/ticket-helper/internal/email"
+	"github.com/wellitonscheer/ticket-helper/internal/utils"
 )
 
 type Login struct{}
@@ -21,11 +22,14 @@ func (l *Login) LoginPage(c *gin.Context) {
 func (l *Login) SendEmailVefificationCode(c *gin.Context) {
 	to := c.PostForm("email")
 
-	err := email.SendEmail(to, "verification code", "your code is 343443")
+	verificationCode := utils.Random6Numbers()
+
+	err := email.SendEmail(to, "verification code", fmt.Sprintf("your code is %d", verificationCode))
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"success": false, "error": fmt.Sprintf("failed to send verification code: %v", err.Error())})
 		return
 	}
 
-	c.HTML(http.StatusOK, "sent-verification-code-success", gin.H{})
+	// TODO: retorna a tela de colocar o email só mudando o botao pra desabilitado
+	c.HTML(http.StatusOK, "sent-verification-code-success", gin.H{"Email": to})
 }
